5660dfb033661e9c63adee8f0d8c4e798f606923
Peter Leung <pleung@hashicorp.com>
2020-04-28 18:08:59 -0400

    Add main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,21 @@ provider "tfe" {
 }
 
 resource "tfe_organization" "test" {
-      name  = "tfe-created-org"
+    name  = "tfe-created-org"
     email = "pleung@hashicorp.com"
+  }
+
+resource "tfe_team" "test" {
+    name         = "tfe-created-team"
+    organization = tfe_organization.test.id
+}
+
+resource "tfe_organization_membership" "test" {
+    organization = tfe_organization.test.id
+    email        = "pleung@hashicorp.com"
+}
+
+resource "tfe_team_organization_member" "test" {
+    team_id                    = tfe_team.test.id
+    organization_membership_id = tfe_organization_membership.test.id
 }
